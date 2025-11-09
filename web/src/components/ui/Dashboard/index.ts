d1f385237ac77ad5ba3ebd7a74aaf41a5ef0259c
Kasper Janowski <2299kasper@gmail.com>
2025-11-09 21:01:29 +0100

    fix of fix

--- web/src/components/ui/Dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as UpcomingPayments } from "./UpcomingPayments";
-export { default as UpcomingEvents } from "./UpcomingEvents";
-export { default as SummaryCards } from "./SummaryCards";
